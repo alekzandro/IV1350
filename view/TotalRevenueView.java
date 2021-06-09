214aa880b4567cea5f8fe2236cf728abf8214a16
alekzandro <58974374+alekzandro@users.noreply.github.com>
2021-06-09 13:48:43 +0200

    Add files via upload

---
+++ view/TotalRevenueView.java
@@ -0,0 +1,31 @@
+package se.kth.iv1350.PointOfSale.view;
+
+import se.kth.iv1350.PointOfSale.model.Observer;
+
+/**
+ * 
+ * Class <code>TotalRevenueView</code> 
+ *
+ */
+public class TotalRevenueView implements Observer {
+	private double totalRevenue = 0;
+	
+	
+	/**
+	 * Updates the total revenue.
+	 * @param totalRevenue The total revenue.
+	 */
+	public void update(double totalRevenue) {
+		this.totalRevenue = totalRevenue;
+		System.out.println("Total revenue: " + totalRevenue);
+	}
+	/**
+	 * Retrieves the total revenue.
+	 * @return The total revenue.
+	 */
+	
+	public double getTotalRevenue() {
+		return totalRevenue;
+	}
+
+}
